c26397442f776e1945064c60d7ae2c4dad245bfb
Yannick Ruijter <ruyter6969@gmail.com>
2025-11-25 22:10:08 +0100

    add clarity to settings file

--- Project/settings.s
+++ Project/settings.s
@@ -1,10 +1,10 @@
-INPUT_COOLDOWN = 15 	; frames
+INPUT_COOLDOWN = 15 	            ; frames
 
-TAB_WIDTH = 4
-PADDING_TOP = 12     ; minimum is preferably 1
-PADDING_LEFT = 2
+TAB_WIDTH = 4                       ; choose width of tab in slides
+PADDING_TOP = 12                    ; number of lines from the top that are left blank (preferably min 1)
+PADDING_LEFT = 2                    ; number of colums left blank at the left side
 ; 0, 1, 2, 3 = red, green, blue, white
-palettes: .byte 0, 1, 2, 3, 3, 3
+palettes: .byte 0, 1, 2, 3, 3, 3    ; putting less indices that slides results in UB
 
-ESCAPE_CHAR = '\'
+ESCAPE_CHAR = '\'                   
 SLIDE_SEPERATOR = 's'
