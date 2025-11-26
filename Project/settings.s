b281cb834511e0f7c9c99ea9770f0563292c70ce
Yannick Ruijter <ruyter6969@gmail.com>
2025-11-26 08:35:21 +0100

    Added ToDo + testing

--- Project/settings.s
+++ Project/settings.s
@@ -1,7 +1,7 @@
 INPUT_COOLDOWN = 15 	            ; frames
 
 TAB_WIDTH = 4                       ; choose width of tab in slides
-PADDING_TOP = 12                    ; number of lines from the top that are left blank (preferably min 1)
+PADDING_TOP = 1                     ; number of lines from the top that are left blank (preferably min 1)
 PADDING_LEFT = 2                    ; number of colums left blank at the left side
 ; 0, 1, 2, 3 = red, green, blue, white
 number_of_slides: .byte 5
